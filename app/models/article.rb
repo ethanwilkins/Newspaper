517963d5e04457b4a5e91387155e89999d1753be
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-07-26 22:06:58 -0400

    card_control_styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  
+  mount_uploader :image, ImageUploader
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :hashtags, dependent: :destroy
+  
+  mount_uploader :image, ImageUploader
 end
