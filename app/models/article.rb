1a4a20baddb494596827c6b14257a3d229ef0bc2
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-08-22 23:13:54 -0400

    ad_views

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,10 +6,15 @@ class Article < ActiveRecord::Base
   def self.local_advert(user)
     local_ads = where(ad: true, zip_code: user.zip_code)
     local_ad = local_ads.limit(Random.new.rand 1..local_ads.size).last if local_ads.size > 0
+    local_ad.view
     return local_ad
   end
   
   def self.ads
     where ad: true
+  end
+  
+  def view
+    update views: views.to_i + 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822222156) do
+ActiveRecord::Schema.define(version: 20140823012732) do
 
   create_table "articles", force: true do |t|
     t.datetime "created_at"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140822222156) do
     t.boolean  "ad"
     t.integer  "zip_code"
     t.string   "hyperlink"
+    t.integer  "views"
   end
 
   create_table "banners", force: true do |t|
