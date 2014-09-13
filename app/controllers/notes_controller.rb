4c8c68c7659daf0be2e1f37c2348e233ec901d1d
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-09-12 20:18:34 -0400

    admin_messages_working

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,15 @@
 class NotesController < ApplicationController
   def notify
     @user = User.find(params[:id])
-    @note = @user.notes.new
+    @note = Note.new
   end
   
   def create
     @note = Note.new(params[:note].permit(:message))
+    @note.user_id = params[:user_id]
+    @note.sender_id = current_user.id
+    @note.action = params[:note_action].to_s
+    @note.item_id = 1
     
     if @note.save
       flash[:notice] = "The notification was sent."
