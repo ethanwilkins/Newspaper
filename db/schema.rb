d7d12ff5bd3f78ede02cd5c8d55c7b98f37fe61d
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-09-26 19:40:47 -0400

    embed_anything

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917233024) do
+ActiveRecord::Schema.define(version: 20140926194850) do
 
   create_table "articles", force: true do |t|
     t.datetime "created_at"
@@ -156,6 +156,13 @@ ActiveRecord::Schema.define(version: 20140917233024) do
     t.boolean  "approved"
     t.integer  "user_id"
     t.boolean  "sponsored"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.text     "text"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
