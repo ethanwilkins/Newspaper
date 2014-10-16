35ccea0d43d355d938af82a2a482fc5b0f6ecd84
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-10-16 19:13:10 -0400

    more_articles_second_try

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,8 +6,11 @@ class WelcomeController < ApplicationController
     end
     session[:more] = nil
     
-    @all_articles = Article.all
-    @articles = Article.all.reverse
+    @articles = Article.all.reverse.
+      # drops first several posts if :feed_page
+      drop((session[:page] ? session[:page] : 0) * page_size).
+      # only shows first several posts of resulting array
+      first(page_size)
     @banner = Banner.last
     @user = User.new
     if current_user
