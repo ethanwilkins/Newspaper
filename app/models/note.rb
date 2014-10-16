7fcd70c7eefcd19194808cb043ea3aba0006e1c2
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-10-16 18:02:35 -0400

    notes_translated

--- app/models/note.rb
+++ app/models/note.rb
@@ -19,23 +19,23 @@ class Note < ActiveRecord::Base
       end
       case action
         when :post_comment
-          message = "#{name} commented on your post."
+          message = name + " " + Translation.translate("commented on your post.")
         when :article_comment
-          message = "#{name} commented on your article."
+          message = name + " " + Translation.translate("commented on your article.")
         when :comment_reply
-          message = "#{name} replied to your comment."
+          message = name + " " + Translation.translate("replied to your comment.")
         when :like_post
-          message = "#{name} liked your post."
+          message = name + " " + Translation.translate("liked your post.")
         when :event_approved
-          message = "Your event was approved."
+          message = Translation.translate("Your event was approved.")
         when :event_denied
-          message = "Your event was denied."
+          message = Translation.translate("Your event was denied.")
         when :tab_approved
-          message = "Your tab was approved."
+          message = Translation.translate("Your tab was approved.")
         when :tab_denied
-          message = "Your tab was denied."
+          message = Translation.translate("Your tab was denied.")
         when :you_won
-          message = "You won! Call 555-5454 now!"
+          message = Translation.translate("You won! Call 555-5454 now!")
       end
       receiver.notes.create!(message: message, sender_id: sender.id,
         action: action.to_s, item_id: item_id)
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,3 +1,12 @@
 class Translation < ActiveRecord::Base
   validates_presence_of :english, :spanish
+  
+  def self.translate(english)
+    spanish = self.where(english: english)
+    return spanish.present? ? spanish.last.spanish : english
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
