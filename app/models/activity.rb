be2bc46badec86a3c0ce82201e4b297b38710bf4
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-11-06 01:45:17 -0500

    unique_locations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,6 +6,17 @@ class Activity < ActiveRecord::Base
   geocoded_by :ip, :latitude => :latitude, :longitude => :longitude
   after_validation :geocode
   
+  def self.unique_locations
+    _unique_locations = []
+    for act in Activity.all
+      unless _unique_locations.any? { |_act| _act.latitude == act.latitude } or \
+        _unique_locations.any? { |_act| _act.longitude == act.longitude }
+        _unique_locations << act
+      end
+    end
+    return _unique_locations
+  end
+  
   def self.log_action(user, ip, action="visit", item_id=nil, data_string=nil)
     if user
       user.activities.create action: action, ip: ip, item_id: item_id, data_string: data_string
