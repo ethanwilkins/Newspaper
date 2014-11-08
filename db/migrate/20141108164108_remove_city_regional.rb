70dea7a314f6399756eaba58dc2745d6baaf1f06
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-11-08 11:55:24 -0500

    act_fields_rm2

--- db/migrate/20141108164108_remove_city_regional.rb
+++ db/migrate/20141108164108_remove_city_regional.rb
@@ -1,7 +1,5 @@
 class RemoveCityRegional < ActiveRecord::Migration
   def change
-    remove_column :activities, :zip_code
-    remove_column :activities, :city
     remove_column :activities, :region_code
   end
 end
