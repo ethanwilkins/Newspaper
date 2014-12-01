c100446f64895f9cf1da6a3da354671483375f4a
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-12-01 00:23:33 -0500

    master_admin_helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  helper_method :current_user, :translate, :page_size, :text_shown
+  helper_method :current_user, :translate, :page_size, :text_shown, :master?, :admin?
 
   private
 
@@ -45,6 +45,14 @@ class ApplicationController < ActionController::Base
   def english?
     (current_user and current_user.english) or (request.host.to_s.include? "elhero.com" and \
       not (current_user and not current_user.english))
+  end
+  
+  def master?
+    return true if current_user and current_user.master
+  end
+  
+  def admin?
+    return true if current_user and current_user.admin
   end
 
   def current_user
