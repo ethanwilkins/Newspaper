2dbc258741469839d5382e410aa02d07eff5ca7e
ethanwilkins <ethanforrestwilkins@gmail.com>
2014-12-29 03:38:01 -0500

    ranked_search_results2

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,14 +2,12 @@ class Search < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :query
   
-  # scan needs to be able to add weight from tags or other
-  # attributes even when first attribute scan returns true
-  # need to separate true/false from adding weight to rank
-  
   def self.scan_users(query)
     results = []
     for user in User.all; rank = [0]
-      if scan(user.name, query, rank) or scan(user.bio, query, rank)
+      scan(user.name, query, rank)
+      scan(user.bio, query, rank)
+      if rank[0] > 0
         results << [user, rank[0]]
       end
     end
@@ -19,7 +17,9 @@ class Search < ActiveRecord::Base
   def self.scan_posts(query)
     results = []
     for post in Post.all; rank = [0]
-      if scan(post.body, query, rank) or scan_hashtags(post, query, rank)
+      scan(post.body, query, rank)
+      scan_hashtags(post, query, rank)
+      if rank[0] > 0
         results << [post, rank[0]]
       end
     end
@@ -29,9 +29,13 @@ class Search < ActiveRecord::Base
   def self.scan_articles(query)
     results = []
     for article in Article.all; rank = [0]
-      if article.ad.nil? and (scan(article.title, query, rank) or \
-        scan(article.body, query, rank) or scan_hashtags(article, query, rank))
-        results << [article, rank[0]]
+      if article.ad.nil?
+        scan(article.title, query, rank)
+        scan(article.body, query, rank)
+        scan_hashtags(article, query, rank)
+        if rank[0] > 0
+          results << [article, rank[0]]
+        end
       end
     end
     return results
@@ -40,7 +44,9 @@ class Search < ActiveRecord::Base
   def self.scan_comments(query)
     results = []
     for comment in Comment.all; rank = [0]
-      if scan(comment.body, query, rank) or scan_hashtags(comment, query, rank)
+      scan(comment.body, query, rank)
+      scan_hashtags(comment, query, rank)
+      if rank[0] > 0
         results << [comment, rank[0]]
       end
     end
@@ -50,7 +56,9 @@ class Search < ActiveRecord::Base
   def self.scan_events(query)
     results = []
     for event in Event.all; rank = [0]
-      if scan(event.title, query, rank) or scan(event.body, query, rank)
+      scan(event.title, query, rank)
+      scan(event.body, query, rank)
+      if rank[0] > 0
         results << [event, rank[0]]
       end
     end
@@ -60,7 +68,9 @@ class Search < ActiveRecord::Base
   def self.scan_tabs(query)
     results = []
     for tab in Tab.all; rank = [0]
-      if scan(tab.name, query, rank) or scan(tab.description, query, rank)
+      scan(tab.name, query, rank)
+      scan(tab.description, query, rank)
+      if rank[0] > 0
         results << [tab, rank[0]]
       end
     end
@@ -68,37 +78,30 @@ class Search < ActiveRecord::Base
   end
   
   def self.scan_hashtags(item, query, rank)
-    found = false
     for tag in item.hashtags
-      if scan(tag.tag, query, rank)
-        found = true
-      end
+      scan(tag.tag, query, rank)
     end
-    return found
-  end
-  
-  def self.scan(text, query, rank)
-    found = false
-    if text.present?
-      for word in text.split(" ")
-        for key_word in query.split(" ")
-          if word.include? key_word.downcase or word.include? key_word.capitalize
-            found = true
-            rank[0] += 1
-          end
-        end
-      end
-    end
-    return found
   end
   
   # will add weight to items that have been
   # chosen often in relation to query
-  def self.scan_searches(query, item)
+  def self.scan_searches(item, query, rank)
     searches = self.where(query: query)
-    if searches
+    if searches.present?
       for search in searches
         
+      end
+    end
+  end
+  
+  def self.scan(text, query, rank)
+    if text.present?
+      for word in text.split(" ")
+        for key_word in query.split(" ")
+          if word.include? key_word.downcase or word.include? key_word.capitalize
+            rank[0] += 1
+          end
+        end
       end
     end
   end
