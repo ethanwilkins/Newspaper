14bd1ca3910f48b9936806eb97fd502ba2403a60
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-02-04 20:25:38 -0500

    commit_script_test12

--- fresh.sh
+++ fresh.sh
@@ -6,11 +6,11 @@ cd /home/rails/
 
 git pull
 
-sh /home/rails/rake db:migrate RAILS_ENV=production
+sh /home/rails/bin/rake db:migrate RAILS_ENV=production
 
-sh /home/rails/rake assets:precompile RAILS_ENV=production
+sh /home/rails/bin/rake assets:precompile RAILS_ENV=production
 
-sh /home/rails/bundle install
+sh /home/rails/bin/bundle install
 
 service unicorn start
 
