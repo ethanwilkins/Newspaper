d342900429de292ef4e51d60f6959ec8c5c2f950
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-03-02 00:43:44 -0500

    redeem_game_board_fix4

--- app/controllers/game_boards_controller.rb
+++ app/controllers/game_boards_controller.rb
@@ -10,6 +10,7 @@ class GameBoardsController < ApplicationController
   def create
     @code = GameBoard.redeem params[:code]
     @group = Group.find_by_user(current_user)
+    @group = @group.is_a? Array ? @group.first : @group # not sure why arrays being returned, hack/fix
     @game_board = current_user.game_boards.new(code_id: @code.id, board_number: @code.board_number,
       zip_code: current_user.zip_code, group_id: @group.id) if @code
     
