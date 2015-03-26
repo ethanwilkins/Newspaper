e74b60b1ce857d4b5831b0f476bc953454fc9c42
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-03-26 03:55:53 -0400

    br_fix

--- bin/bash/fresh.sh
+++ bin/bash/fresh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh root@elheroe.net 'service unicorn stop && cd /home/rails && git pull && export PATH=$PATH:/usr/local/rvm/rubies/ruby-2.1.5/bin && /home/rails/bin/bundle install && /home/rails/bin/rake db:migrate RAILS_ENV=production && /home/rails/bin/rake assets:precompile RAILS_ENV=production && service unicorn start'
+ssh root@elheroe.net 'service unicorn stop && cd /home/rails && git pull && export PATH=$PATH:/usr/local/rvm/rubies/ruby-2.1.5/bin && /home/rails/bin/bundle install && export RAILS_DB_PWD="$RAILS_DB_PWD" && /home/rails/bin/rake db:migrate RAILS_ENV=production && /home/rails/bin/rake assets:precompile RAILS_ENV=production && service unicorn start'
