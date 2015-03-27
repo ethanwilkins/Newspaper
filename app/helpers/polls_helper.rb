56d34cf79a58e43bd75fe6ae7f6ce5da5851203a
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-03-27 00:27:24 -0400

    polls5

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,2 +1,9 @@
 module PollsHelper
+  def up_voted? choice
+    choice.votes.where(voter_id: current_user.id).where(up: true).present?
+  end
+  
+  def down_voted? choice
+    choice.votes.where(voter_id: current_user.id).where(down: true).present?
+  end
 end
