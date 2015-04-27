655076a1c01335a99b0e2506fcc3a27bd1b1e276
ethanwilkins <ethanforrestwilkins@gmail.com>
2015-04-27 19:50:01 -0400

    one_more_match

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -15,23 +15,36 @@ class Tournament < ActiveRecord::Base
   			self.members.create(sports_team_id: value)
   		end
   	end
-  	
     # creates team pairs, best with worst
     teams = self.teams.sort_by { |team| team.points }
+    teams_size = teams.size # to be later validated
     pairs = (teams.size/2).times.map do
     	[teams.shift, teams.pop]
     end
-    
     # sets number of rounds
 		self.update total_rounds: (self.teams.size / 2.to_f).ceil.to_i
     
-    # inserts team pairs into matches
+    # inserts teams into matches
     for pair in pairs
       break if self.matches.size.eql? self.teams.size - 1 # ensures correct num of matches
-  		match = self.matches.create round: self.total_rounds - (pairs.index(pair) + 1)
+  		match = self.matches.create round: self.get_round pairs.index(pair)
   		match.members.create sports_team_id: pair.first.id
   		match.members.create sports_team_id: pair.last.id
   	end
+    # inserts more if any missing
+    if teams.present?
+      match = self.matches.create round: self.total_rounds
+      match.members.create sports_team_id: teams.last.id
+    elsif teams_size.even?
+      ((teams_size - 1) - self.matches.size).times do
+        self.matches.create round: self.total_rounds
+      end
+    end
+	end
+	
+	# gets correct round for each match
+	# factors: team size, match size, total_rounds, index
+	def get_round index
 	end
   
   def teams
